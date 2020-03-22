d6710d6db5ec6d28395768e0453d6193c9d37763
Mirco Zeiss <mirco.zeiss@gmail.com>
2020-03-22 22:40:36 +0100

    start working on application

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -22,7 +22,7 @@ import Subscription from '../../dist/Subscription'
 import { TypeIdString, AttributeIdEventNotifier } from '../../dist/ua/enums'
 import Variant from '../../dist/ua/Variant'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 ;(async function() {
   const client = new Client('ws://localhost:1234')
@@ -147,10 +147,128 @@ import { render } from 'react-dom'
   console.log(triggerResponse)
 })()
 
-class HelloMessage extends React.Component {
-  render() {
-    return <div>quite</div>
+import './style.scss'
+
+// const Folder = () => (
+//   <svg
+//     width="1em"
+//     height="1em"
+//     viewBox="0 0 16 16"
+//     fill="currentColor"
+//     xmlns="http://www.w3.org/2000/svg"
+//   >
+//     <path d="M9.828 4a3 3 0 01-2.12-.879l-.83-.828A1 1 0 006.173 2H2.5a1 1 0 00-1 .981L1.546 4h-1L.5 3a2 2 0 012-2h3.672a2 2 0 011.414.586l.828.828A2 2 0 009.828 3v1z" />
+//     <path
+//       fillRule="evenodd"
+//       d="M13.81 4H2.19a1 1 0 00-.996 1.09l.637 7a1 1 0 00.995.91h10.348a1 1 0 00.995-.91l.637-7A1 1 0 0013.81 4zM2.19 3A2 2 0 00.198 5.181l.637 7A2 2 0 002.826 14h10.348a2 2 0 001.991-1.819l.637-7A2 2 0 0013.81 3H2.19z"
+//       clipRule="evenodd"
+//     />
+//   </svg>
+// )
+
+const FolderPlus = () => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M9.828 4H2.19a1 1 0 00-.996 1.09l.637 7a1 1 0 00.995.91H9v1H2.826a2 2 0 01-1.991-1.819l-.637-7a1.99 1.99 0 01.342-1.31L.5 3a2 2 0 012-2h3.672a2 2 0 011.414.586l.828.828A2 2 0 009.828 3h3.982a2 2 0 011.992 2.181L15.546 8H14.54l.265-2.91A1 1 0 0013.81 4H9.828zm-2.95-1.707L7.587 3H2.19c-.24 0-.47.042-.684.12L1.5 2.98a1 1 0 011-.98h3.672a1 1 0 01.707.293z"
+      clipRule="evenodd"
+    />
+    <path
+      fillRule="evenodd"
+      d="M13.5 10a.5.5 0 01.5.5v2a.5.5 0 01-.5.5h-2a.5.5 0 010-1H13v-1.5a.5.5 0 01.5-.5z"
+      clipRule="evenodd"
+    />
+    <path
+      fillRule="evenodd"
+      d="M13 12.5a.5.5 0 01.5-.5h2a.5.5 0 010 1H14v1.5a.5.5 0 01-1 0v-2z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+
+const FolderMinus = () => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M9.828 4H2.19a1 1 0 00-.996 1.09l.637 7a1 1 0 00.995.91H9v1H2.826a2 2 0 01-1.991-1.819l-.637-7a1.99 1.99 0 01.342-1.31L.5 3a2 2 0 012-2h3.672a2 2 0 011.414.586l.828.828A2 2 0 009.828 3h3.982a2 2 0 011.992 2.181L15.546 8H14.54l.265-2.91A1 1 0 0013.81 4H9.828zm-2.95-1.707L7.587 3H2.19c-.24 0-.47.042-.684.12L1.5 2.98a1 1 0 011-.98h3.672a1 1 0 01.707.293z"
+      clipRule="evenodd"
+    />
+    <path
+      fillRule="evenodd"
+      d="M11 11.5a.5.5 0 01.5-.5h4a.5.5 0 010 1h-4a.5.5 0 01-.5-.5z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+
+const App = () => {
+  // const []
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onClick = () => {
+    setIsOpen(!isOpen)
   }
+
+  return (
+    <div className="wrapper">
+      <div className="left bg-light">
+        <div className="list-group">
+          <a
+            href="#item-1-1"
+            className="list-group-item"
+            data-toggle="collapse"
+            onClick={onClick}
+          >
+            <div className="d-flex align-items-center">
+              {isOpen ? <FolderMinus /> : <FolderPlus />}
+              <span className="ml-2">Root Folder</span>
+            </div>
+          </a>
+          <div className="list-group collapse" id="item-1-1">
+            <a
+              href="#objects"
+              className="list-group-item"
+              data-toggle="collapse"
+            >
+              Objects
+            </a>
+            <div className="list-group collapse" id="objects">
+              <a href="#" className="list-group-item">
+                Server
+              </a>
+            </div>
+            <a href="#" className="list-group-item">
+              Types
+            </a>
+            <a href="#" className="list-group-item">
+              Views
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="content">
+        some content
+        <button type="button" className="btn btn-primary">
+          hello
+        </button>
+      </div>
+      <div className="right">right</div>
+      <div className="footer">some footer</div>
+    </div>
+  )
 }
 
-render(<HelloMessage />, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -12,7 +12,20 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js']
   },
   module: {
-    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }]
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
